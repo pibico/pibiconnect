81a6ad9b171902077bd3ff839ffc6fda9f43bed1
pibiKoin <segitrack@gmail.com>
2024-11-15 15:22:42 +0100

    modified:   pibiconnect/fixtures/custom_html_block.json 	modified:   pibiconnect/pibiconnect/doctype/cn_span/cn_span.py

--- pibiconnect/pibiconnect/doctype/cn_span/cn_span.py
+++ pibiconnect/pibiconnect/doctype/cn_span/cn_span.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2024, pibiCo and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class CNSpan(Document):
-	pass
+    def validate(self):
+        if not self.higher_span and not self.lower_span:
+            self.span_factor = 1
+            return
+            
+        value_range = self.higher_span - self.lower_span
+        self.span_factor = value_range / 10
+        
+    def calculate_reading(self, voltage):
+        """
+        Calculate sensor reading from voltage input (0-10V)
+        If no spans defined, returns raw voltage value
+        Includes calibration factor offset of 0.30V
+        """
+        if not self.higher_span and not self.lower_span:
+            return voltage
+            
+        calibration_factor = 0.30
+        return self.lower_span + ((voltage - calibration_factor) * self.span_factor)
